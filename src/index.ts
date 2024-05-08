b9c540b29660a30582cba4433e8387f6d29b118a
itsjohncs <johnsullivan.pem@gmail.com>
2024-05-07 18:53:44 -0700

    Generate placeholder list of tags.
    This is a temporary approach to get up and going. I eventually want to
    have a list of tags maintained in this repo. That way I can ensure that
    the list of tags is curated and the aliases are populated reasonably.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import Asset from "./Asset.js";
+import Tag from "./Tag.js";
 import {buildIndex as buildTooManyTokensIndex} from "./indexers/tooManyTokens.js";
 
-export async function buildIndex(): Promise<Asset[]> {
-    return buildTooManyTokensIndex();
+interface Index {
+    assets: Asset[];
+    tags: Tag[];
+}
+
+export async function buildIndex(): Promise<Index> {
+    const assets = await buildTooManyTokensIndex();
+
+    const tags: Tag[] = [];
+    const allNamedTags = new Set<string>(assets.flatMap((asset) => asset.tags));
+    for (const name of allNamedTags) {
+        tags.push({
+            canonical: [name],
+            aliases: [],
+        });
+    }
+
+    return {
+        assets,
+        tags,
+    };
 }
