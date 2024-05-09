ac1c96b175c4cc3352e6b66b5c4780699e46f227
itsjohncs <johnsullivan.pem@gmail.com>
2024-05-09 13:34:48 -0700

    Get Foundry API types installed.

--- packages/foundry-search-module/src/index.ts
+++ packages/foundry-search-module/src/index.ts
@@ -1,3 +1,13 @@
-import getHelloText from "./getHelloText";
+console.log("Hello World! This code runs immediately when the file is loaded.");
 
-console.log(getHelloText());
+Hooks.on("init", function () {
+    console.log(
+        "This code runs once the Foundry VTT software begins its initialization workflow.",
+    );
+});
+
+Hooks.on("ready", function () {
+    console.log(
+        "This code runs once core initialization is ready and game data is available.",
+    );
+});
